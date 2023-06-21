a8f77559a079cd2827a8ae00fe1c101167231aa7
Victoriaolu <victoriaoluchiamakoh@gmail.com>
2023-06-20 18:09:47 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include “main.h”
+
+/**
+*main – prints _putchar,  followed by a new line.
+* Return: 0
+**/
+
+Int main(void)
+{
+  Unsigned int ;
+  Char c[] = “_putchar”;
+
+  For (i=0;I<=7;i++)
+  { 
+    _putchar(c[i]);
+  } 
+
+   _putchar(‘/n’);
+
+   Return (0);
+{
+
