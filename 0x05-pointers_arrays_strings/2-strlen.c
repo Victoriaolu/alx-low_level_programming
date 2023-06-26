f39e2ffff1bee6a24f2bc1e5c81cf85bde22dcfb
Victoriaolu <victoriaoluchiamakoh@gmail.com>
2023-06-26 12:44:28 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _stren - returns the length of @s
+ * @s: the string
+ * Return:lenght of @s
+ * */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++);
+
+	return (i);
+}
