f2c060ff336756d28e4869d30cb29b61da22170f
Victoriaolu <victoriaoluchiamakoh@gmail.com>
2023-07-05 17:05:37 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion: the main entry
+ * @s: the input
+ */
+void _puts_recursion(char *s)
+{
+	if (*(s) != '\0')
+	{
+		_putchar(*s);
+		_put_recursion(s+1);
+	}
+	if (*(s) == '\0')
+	{
+		_putchar('\n');
+	}
+}
