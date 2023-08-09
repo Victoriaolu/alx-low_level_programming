546d6dde9178b82d81303ae0e5b676116fa032c1
Victoriaolu <victoriaoluchiamakoh@gmail.com>
2023-08-08 17:35:40 -0700

    Tread lightly

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - its reads a text file and print the letters.
+ * @filename:the filename.
+ * @letters:the number of letters to be printed.
+ *
+ * Return:the numbers of letters to be printed.if it fails, return 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
